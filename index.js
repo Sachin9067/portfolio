f03d0eb04ef9b734dcd283e28ec626b83804652f
Sachin9067 <124483990+Sachin9067@users.noreply.github.com>
2023-03-25 19:26:44 +0530

    index.html

---
+++ index.js
@@ -0,0 +1,9 @@
+var typed=new typed("span.multiple-text", 
+{
+    String: ["Web Development","Programming","Problem solving"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+}
+)
